8d0e5a3b78fa9e666b8d9354152fa5cddfc4cac5
Aidan Killeen <aidan.killeen@gmail.com>
2022-02-08 10:56:15 +0000

    added CommonBaseClass test

---
+++ CommonBaseClassTest/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonBaseClassTest
+{
+    class Person : Object
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return "Person:" + FirstName + " " + LastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // Console.WriteLine("Person.Equals called");
+            if ((obj == null || !this.GetType().Equals(obj.GetType())))
+            {
+                return false;
+            }
+
+            Person p = (Person)obj;
+
+            if (p.FirstName.Equals(FirstName) && p.LastName.Equals(LastName)) {
+                return true;
+            } else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ CommonBaseClassTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CommonBaseClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Common Base Class");
+
+            Person p = new Person("Alice", "Andrews");
+
+            Console.WriteLine(p);
+
+            Person p2 = new Person("Alice", "Andrews");
+            //Person p2 = p;
+
+            // never use == comparison
+            // use .Equals instead!
+            if (p.Equals(p2))
+            {
+                Console.WriteLine("The same");
+            } else
+            {
+                Console.WriteLine("Different");
+            }
+
+        }
+    }
+}
