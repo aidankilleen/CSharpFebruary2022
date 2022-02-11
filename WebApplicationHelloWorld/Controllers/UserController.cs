f034e95c0cd0f4f6df682c8c4c75f6af8d25fb2e
Aidan Killeen <aidan.killeen@gmail.com>
2022-02-11 14:50:57 +0000

    added MVC Web Application

---
+++ WebApplicationHelloWorld/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserDaoLibrary;
+
+namespace WebApplicationHelloWorld
+{
+    public class UserController : Controller
+    {
+        private UserDao dao = new UserDao();
+
+        public IActionResult Index()
+        {
+            // 
+            List<User> users = dao.GetUsers();
+
+            // TODO send the users to the View...
+            ViewBag.message = "User List";
+            ViewBag.users = users;
+
+            return View();
+        }
+    }
+}
